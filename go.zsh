e45fb4ab587f784374ddbc6065d32eeea4b0c8ec
Harley Laue <losinggeneration@gmail.com>
2023-02-23 17:39:51 -0800

    Better quote Go variables for paths that could have spaces
    Typically I'm not in an environment where this is an issue, but quoting
    these is safer and doesn't hurt anything.

--- go.zsh
+++ go.zsh
@@ -1,9 +1,10 @@
 # Google Go Programming Language
 if [ "$(command -v go)" ]; then
-	export GOROOT_BOOTSTRAP=$(dirname $(go env GOROOT))/src-bootstrap
-	export GOPATH=$HOME/Source/languages/go/ext
+	export GOROOT_BOOTSTRAP="$(dirname $(go env GOROOT))/src-bootstrap"
+	export GOPATH="$HOME/Source/languages/go/ext"
 	#export GOOS=linux
 	#export GOARCH=amd64
-	export GOBIN=$HOME/Programs/bin
-	[ -f "$(go env GOROOT)/misc/zsh/go" ] && source $(go env GOROOT)/misc/zsh/go
+	export GOBIN="$HOME/Programs/bin"
+	[ -f "$(go env GOROOT)/misc/zsh/go" ] && source "$(go env GOROOT)/misc/zsh/go"
+	appendpath "$GOBIN"
 fi
