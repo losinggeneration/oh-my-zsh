d923e906c90b467307efaee626020201f2afc419
Harley Laue <losinggeneration@gmail.com>
2025-12-15 16:41:11 -0800

    Change where GOBIN is

--- go.zsh
+++ go.zsh
@@ -4,7 +4,7 @@ if [ "$(command -v go)" ]; then
 	export GOPATH="$HOME/Source/languages/go/ext"
 	#export GOOS=linux
 	#export GOARCH=amd64
-	export GOBIN="$HOME/Programs/bin"
+	export GOBIN="$HOME/.local/share/go/bin"
 	[ -f "$(go env GOROOT)/misc/zsh/go" ] && source "$(go env GOROOT)/misc/zsh/go"
 	appendpath "$GOBIN"
 fi
